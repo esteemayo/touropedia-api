1d0e67309b7726ce6db5e05599caee7514739de9
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-06-03 16:11:07 +0100

    modified

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -21,11 +21,16 @@ const mongoURI = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
+const devEnv = process.env.NODE_ENV !== 'production';
+
 // MongoDB connection
 mongoose
-  .connect(mongoURI)
+  .connect(`${devEnv ? dbLocal : mongoURI}`)
   .then(() =>
-    console.log(`Connnected to MongoDB Successfully → ${mongoURI}`.gray.bold)
+    console.log(
+      `Connnected to MongoDB Successfully → ${devEnv ? dbLocal : mongoURI}`.gray
+        .bold
+    )
   )
   .catch((err) =>
     console.log(`Could not connect to MongoDB → ${err}`.red.bold)
